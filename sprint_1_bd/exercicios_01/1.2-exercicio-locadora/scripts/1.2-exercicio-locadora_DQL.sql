ad5cbe2de509fe5d3f5129ec90fd8f8519ac94e5
Samuel <samuel.figs@gmail.com>
2021-08-10 11:13:01 -0300

    dql 1.2 OK

--- sprint_1_bd/exercicios_01/1.2-exercicio-locadora/scripts/1.2-exercicio-locadora_DQL.sql
+++ sprint_1_bd/exercicios_01/1.2-exercicio-locadora/scripts/1.2-exercicio-locadora_DQL.sql
@@ -9,18 +9,34 @@ SELECT * FROM MARCA;
 SELECT * FROM MODELO;
 SELECT * FROM VEICULO;
 
-SELECT valorAluguel, dataRetirada, dataDevolucao, nomeCliente FROM ALUGUEL
-LEFT JOIN CLIENTE
-ON ALUGUEL.idCliente = CLIENTE.idCliente;
-SELECT nomeModelo FROM VEICULO
-LEFT JOIN MODELO
-ON VEICULO.IdModelo = MODELO.idModelo
 
-/*
-SELECT valorAluguel, dataRetirada, dataDevolucao, nomeCliente FROM ALUGUEL
-LEFT JOIN CLIENTE
-ON ALUGUEL.idCliente = CLIENTE.idCliente;
-*/
+-- listar todos os alugueis mostrando as datas de início e fim, o nome do cliente que alugou e nome do modelo do carro
+
+SELECT valorAluguel AS 'Valor em R$', dataRetirada AS 'Data de Retirada', dataDevolucao as 'Data de Devolução', 
+nomeCliente AS 'Nome do Cliente', nomeModelo AS 'Modelo' FROM ALUGUEL
+INNER JOIN CLIENTE
+ON ALUGUEL.idCliente = CLIENTE.idCliente
+INNER JOIN VEICULO
+ON VEICULO.IdVeiculo = ALUGUEL.idVeiculo
+INNER JOIN MODELO
+ON VEICULO.idModelo = MODELO.idModelo
+
+
+-- listar os alugueis de um determinado cliente mostrando seu nome, as datas de início e fim e o nome do modelo do carro
+
+SELECT CLIENTE.idCliente, nomeCliente, nomeModelo, dataRetirada, dataDevolucao, valorAluguel FROM CLIENTE
+
+INNER JOIN ALUGUEL
+ON CLIENTE.idCliente = ALUGUEL.idCliente
+
+INNER JOIN VEICULO
+ON ALUGUEL.idVeiculo = VEICULO.idVeiculo
+
+INNER JOIN MODELO
+ON MODELO.idModelo = VEICULO.idModelo
+
+WHERE CLIENTE.idCliente = 2
+
 
 
 
