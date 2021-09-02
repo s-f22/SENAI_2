057cd4bc6756540f4cb556c260c37501964ed1b9
Samuel <samuel.figs@gmail.com>
2021-09-02 11:32:20 -0300

    rental

---
+++ sprint_2_backEnd/rental/senai_rental_webAPI/Senai.Rental.WebApi.Samuel/Senai.Rental.WebApi.Samuel/Senai.Rental.WebApi.Samuel/Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Rental.WebApi_Samuel.Domains;
+using Senai.Rental.WebApi_Samuel.Interfaces;
+using Senai.Rental.WebApi_Samuel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebApi_Samuel.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private IClienteRepository _clienteRepository { get; set; }
+
+        public ClientesController()
+        {
+            _clienteRepository = new ClienteRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<ClienteDomain> listaClientes = _clienteRepository.Listar();
+
+            return Ok(listaClientes);
+        }
+
+    }
+}
