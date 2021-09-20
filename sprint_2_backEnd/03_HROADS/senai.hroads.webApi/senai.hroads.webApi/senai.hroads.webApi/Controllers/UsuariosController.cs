33575ca887a16a05ba585ac24b9380730e1239b7
Pedro Cabral <pedro.intropedi@gmail.com>
2021-09-19 22:47:09 -0300

    Problema resolvido saboroso

--- sprint_2_backEnd/03_HROADS/senai.hroads.webApi/senai.hroads.webApi/senai.hroads.webApi/Controllers/UsuariosController.cs
+++ sprint_2_backEnd/03_HROADS/senai.hroads.webApi/senai.hroads.webApi/senai.hroads.webApi/Controllers/UsuariosController.cs
@@ -1,15 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using senai.hroads.webApi.Domains;
+using senai.hroads.webApi.Interfaces;
+using senai.hroads.webApi.Repositories;
 
 namespace senai.hroads.webApi.Controllers
 {
-    [Produces ("application/json")]
+    [Produces("application/json")]
 
     [Route("api/[controller]")]
     [ApiController]
     public class UsuariosController : ControllerBase
     {
-        
+        private IUsuarioRepository _usuarioRepository { get; set; }
 
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
 
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_usuarioRepository.ListarTodos());
+        }
+
+        [HttpGet("{idUsuario}")]
+        public IActionResult BuscarPorId(int idUsuario)
+        {
+            return Ok(_usuarioRepository.BuscarPorId(idUsuario));
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario novoUsuario)
+        {
+            _usuarioRepository.Cadastrar(novoUsuario);
+
+            return StatusCode(201);
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(Usuario usuarioAtualizado)
+        {
+            _usuarioRepository.Atualizar(usuarioAtualizado);
+
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{idUsuario}")]
+        public IActionResult Deletar(int idUsuario)
+        {
+            _usuarioRepository.Deletar(idUsuario);
+
+            return StatusCode(204);
+        }
     }
 }
